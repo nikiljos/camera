b7aefd911a6ac1d68f7df4f6dc4c03c93a852547
nikiljos <nikhiljose12345@gmail.com>
2024-11-30 23:41:43 +0530

    feat: mic check support; chore: refactor code

--- video.js
+++ video.js
@@ -1,34 +1,89 @@
+const streamSource = {
+    mic: null,
+    camera: null,
+};
+const $videoDiv = document.getElementById("videoDiv");
+const $audioDiv = document.getElementById("audioDiv");
+const $startVideoBtn = document.getElementById("startVideoBtn");
+const $startAudioBtn = document.getElementById("startAudioBtn");
+const $stopVideoBtn = document.getElementById("stopVideoBtn");
+const $stopAudioBtn = document.getElementById("stopAudioBtn");
+let currentLoudness = null;
+
 function startVideo(id) {
     //Selector for your <video> element
-    document.getElementById(
-        "videoDiv"
-    ).innerHTML = `<video id="myVidPlayer" hide-controls muted autoplay></video>`;
-    const video = document.querySelector("#myVidPlayer");
+    stopVideo();
+    const video = document.createElement("video");
+    video.setAttribute("id", "myVidPlayer");
+    video.setAttribute("hide-controls", "");
+    video.setAttribute("muted", "");
+    video.setAttribute("autoplay", "");
+    $videoDiv.appendChild(video);
     window.navigator.mediaDevices
         .getUserMedia({
             audio: false,
             video: { width: 1920, height: 1080, deviceId: id },
         })
         .then((stream) => {
+            streamSource.camera = stream;
             video.srcObject = stream;
             console.log(stream);
             video.onloadedmetadata = (e) => {
                 video.play();
             };
         })
-        .catch(() => {
-            alert("You have give browser the permission to run Webcam and mic ;( ");
+        .catch((error) => {
+            console.error("Error accessing video devices:", error);
         });
 }
 
-listDevices();
-function listDevices() {
+function logLoudness(analyser, dataArray) {
+    if (!streamSource.mic) {
+        //stop recuirsion when stream is clear
+        return;
+    }
+    analyser.getByteFrequencyData(dataArray);
+    const sum = dataArray.reduce((a, b) => a + b, 0);
+    const average = sum / dataArray.length;
+    const newVal = Math.ceil(average);
+    if (currentLoudness != newVal) {
+        $audioDiv.innerHTML = "🔈 " + "❚".repeat(newVal);
+    }
+    // console.log("Loudness:", average);
+    requestAnimationFrame(() => logLoudness(analyser, dataArray));
+}
+
+function startAudio(id) {
+    // Create an audio context
+    stopAudio();
+    const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)();
+    const analyser = audioContext.createAnalyser();
+
+    // Get user media for audio
+    navigator.mediaDevices
+        .getUserMedia({ audio: { deviceId: id } })
+        .then((stream) => {
+            streamSource.mic = stream;
+            const microphone = audioContext.createMediaStreamSource(stream);
+            microphone.connect(analyser);
+
+            // Create a buffer to hold the data
+            const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+            // Start logging loudness
+            logLoudness(analyser, dataArray);
+        })
+        .catch((error) => {
+            console.error("Error accessing audio devices:", error);
+        });
+}
+
+function listCameras() {
     if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
         console.log("enumerateDevices() not supported.");
         return;
     }
-
-    // List cameras and microphones.
 
     navigator.mediaDevices
         .enumerateDevices()
@@ -38,7 +93,6 @@ function listDevices() {
                     document.getElementById(
                         "camSelector"
                     ).innerHTML += `<option value="${device.deviceId}">${device.label}</option>`;
-                // console.log(device.kind + ": " + device.label + " id = " + device.deviceId);
             });
         })
         .catch(function (e) {
@@ -46,10 +100,61 @@ function listDevices() {
         });
 }
 
+function listMics() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+        console.log("enumerateDevices() not supported.");
+        return;
+    }
+
+    // List microphones.
+    navigator.mediaDevices
+        .enumerateDevices()
+        .then(function (devices) {
+            devices.forEach(function (device) {
+                if (device.kind == "audioinput")
+                    document.getElementById(
+                        "micSelector"
+                    ).innerHTML += `<option value="${device.deviceId}">${device.label}</option>`;
+            });
+        })
+        .catch(function (e) {
+            console.log(e.name + ": " + e.message);
+        });
+}
+
+const stopStream = (stream) => {
+    if (stream) {
+        stream.getTracks().forEach((track) => track.stop());
+    }
+};
+
 function stopVideo() {
-    location.reload();
+    stopStream(streamSource.camera);
+    streamSource.camera = null;
+    $videoDiv.innerHTML = "";
+}
+function stopAudio() {
+    stopStream(streamSource.mic);
+    streamSource.mic = null;
+    $audioDiv.innerHTML = "";
 }
 
 function fullScreen() {
     document.getElementById("videoDiv").webkitRequestFullScreen();
 }
+
+window.onload = (e) => {
+    listCameras();
+    listMics();
+};
+
+$startVideoBtn.onclick = (e) => {
+    startVideo(document.getElementById("camSelector").value);
+};
+
+$startAudioBtn.onclick = (e) => {
+    startAudio(document.getElementById("micSelector").value);
+};
+
+$stopVideoBtn.onclick = stopVideo;
+$stopAudioBtn.onclick = stopAudio;
